20e1339ffe59c93b74a2b0659ef6804cff85b035
Michael_Fesenko <@gmail.com>
2023-07-12 14:50:56 +0300

    добавил Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,111 @@
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Вариант 1. Введенное число обрабатывается как массив символов.
+//            К строке можно обращаться как к массиву.
+//            Работает для чисел и слов любой длины. Проверок на ошибки нет.
+// Console.Write("Введите число: ");
+// string input = Console.ReadLine();
+// int n = input.Length;
+// bool flag = true;
+//
+// for (int i = 0; i < n / 2; i++)
+// {
+//     if (input[i] != input[n - 1 - i])
+//     {
+//         flag = false;
+//         break;
+//     }
+// }
+//
+// if (flag)
+// {
+//     Console.Write("да");
+// }
+// else
+// {
+//     Console.Write("нет");
+// }
+
+// Вариант 2. Это вариант 1 с функциями.
+void ShowResult(bool value) // Выводит текстовое сообщение
+{
+    if (value)
+    {
+        Console.WriteLine("Это палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Это не палиндром");
+    }
+}
+
+bool isPalindrome(string number) // Определяет является ли число (слово) палиндромом
+{
+    for (int i = 0; i < number.Length / 2; i++)
+    {
+        if (number[i] != number[number.Length - 1 - i])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+string GetInput(string text) // Выдает запрос на ввод числа (слова)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+string input = GetInput("Введите число или слово: ");
+ShowResult(isPalindrome(input));
+
+
+
+
+
+
+
+
+
+
+
+
+// ChatGPT
+// using System;
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         Console.Write("Enter a five-digit number: ");
+//         string input = Console.ReadLine();
+
+//         if (input.Length != 5)
+//         {
+//             Console.WriteLine("Invalid input. Please enter a five-digit number.");
+//             return;
+//         }
+
+//         bool isPalindrome = IsPalindrome(input);
+
+//         if (isPalindrome)
+//             Console.WriteLine("The number is a palindrome.");
+//         else
+//             Console.WriteLine("The number is not a palindrome.");
+//     }
+
+//     static bool IsPalindrome(string number)
+//     {
+//         for (int i = 0; i < number.Length / 2; i++)
+//         {
+//             if (number[i] != number[number.Length - 1 - i])
+//                 return false;
+//         }
+//         return true;
+//     }
+// }
